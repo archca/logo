1dd89ece54b410a02404d06346b3def18b37a493
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2019-05-14 11:03:00 +0200

    logo vorschlag MH

---
+++ logo_mh.R
@@ -0,0 +1,11 @@
+library(hexSticker)
+
+imgurl <- "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Triskele-Symbol1.svg/822px-Triskele-Symbol1.svg.png"
+
+sticker(imgurl, package="ACA BeRn", p_size=15, p_y = 0.6,
+        s_x=1, s_y=1.2,  # position
+        s_width=.5,
+        h_fill = "white", h_color="#E81B00", p_color = "#E81B00",
+        url = "quantitative archaeology – swissmade",
+        u_size = 4.05,
+        filename="logo_mh.png")
